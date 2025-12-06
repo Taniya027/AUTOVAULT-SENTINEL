54652d42eaf33bb7b3c1cf803d2715ab1f1850d8
Taniya027 <taniya272003@gmail.com>
2025-12-06 19:01:41 +0530

    "Configured Stagenet network in Hardhat"

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -34,5 +34,11 @@ export default defineConfig({
       url: configVariable("SEPOLIA_RPC_URL"),
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
+    stagenet: {
+      type: "http",
+      chainType: "l1",
+      url: "https://rpc.contract.dev/18c9a42c6057e1c17abb80167978c5f3",
+      accounts: ["0xf45909d51d94e639f77fe21c08347b2c5eaaa9bee823499f4570eb7b397ab207"],
+    },
   },
 });
